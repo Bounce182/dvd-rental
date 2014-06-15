2de17efc54e753ecef2c06933749008aa167d767
Vytautas Civinskas <info@anv.lt>
2014-06-15 16:11:57 +0300

    Rentals pagination added

--- ror-app/app/controllers/rentals_controller.rb
+++ ror-app/app/controllers/rentals_controller.rb
@@ -6,7 +6,7 @@ class RentalsController < ApplicationController
   # GET /rentals
   # GET /rentals.json
   def index
-    @rentals = Rental.all
+    @rentals = Rental.all.paginate(:page => params[:page], :per_page => 5)
   end
 
   # GET /rentals/1
